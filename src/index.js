8ebb66672ff97c8f07a6d96f995a47934529b37e
alivar391 <patriot391@mail.ru>
2021-07-17 17:34:07 +0300

    feat: update index.js in src

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let text = String(Math.abs(n));
+    let output = '';
+    for (let i = text.length-1; i >= 0; i--){
+        output = output + text[i];
+    }
+    return output;
 }
